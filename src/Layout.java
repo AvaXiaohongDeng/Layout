8447a9b89147b0c6ecdbb14182cedfa9e9f96e1f
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2020-11-28 19:13:42 -0500

    practising how to layout components in a window(ContentPane) using layout manager classes(GridLayout/FlowLayout/BoxLayout...).

---
+++ src/Layout.java
@@ -0,0 +1,42 @@
+/*java.awt package(abstract window toolkit)contains many layout manager classes(
+  GridLayout/GroupLayout/SpringLayout/GridBagLayout/FlowLayout/CardLayout
+  /BoxLayout/BorderLayout) that can be used to place components in a
+  container-ContentPane
+  )
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Layout extends JFrame {
+    JPanel pnl = new JPanel();
+    Container contentPane = getContentPane();
+    JPanel grid = new JPanel(new GridLayout(2,2));
+
+    public Layout(){
+      super("Swing Window");
+      setSize(500,600);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+      pnl.add(new JButton("Yes"));
+      pnl.add(new JButton("No"));
+      pnl.add(new JButton("Cancel"));
+
+      grid.add(new JButton("1"));
+      grid.add(new JButton("2"));
+      grid.add(new JButton("3"));
+      grid.add(new JButton("4"));
+
+      contentPane.add("North",pnl);
+      contentPane.add("Center",grid);
+      contentPane.add("West",new JButton("West"));
+      contentPane.add("East",new JButton("East"));
+      contentPane.add("South",new JButton("South"));
+
+      setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Layout gui = new Layout();
+    }
+}
